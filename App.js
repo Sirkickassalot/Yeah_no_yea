9afff6c2a39ba3038633bc6e19b2579131735d7e
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2021-11-21 21:47:30 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+// let yeah = 'yeah';
+// let no = 'no';
+// let noYeah = 'sure';
+// let yeahNo = 'no';
+// let yeahNoforsure = 'yeah';
+// let yeahYeahyeah = 'no';
+// let yeahNoyeah = 'im willing to do it';
+// let noYeahno = 'There is no way I am doing it';
+
+
+// let poop = document.getElementsByClassName("btn") 
+// poop.addEventListener("click", buttonFunction(){
+//   console.log(document.getElementById("input_yeah").placeholder = document.getElementsByClassName("btn").value
+// });
+
+let yea = document.getElementsByClassName("btn")
+function buttonFunction() { 
+  for (let i = 0; i < yea.length; i++){
+    // console.log(document.getElementsByClassName("btn")[i].title)
+     console.log(yea[i].getAttribute(title))
+  }
+  
+
+  document.getElementById("input_yeah").placeholder = document.getElementsByClassName("btn") 
+  // console.log(document.getElementsByClassName("btn"))
+  }
+
+
+// let yeahButtons = document.getElementById('Btn').value;
+  // document.getElementById('input_yeah')= yeahButtons;
+  // return buttonFunction()
+
+//button addeventlistener
